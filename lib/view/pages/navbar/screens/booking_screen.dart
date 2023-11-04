a9540804b213b3836e1335761ac268af00b04915
mohidhafeez <mohidzohaib545@gmail.com>
2023-11-04 18:54:08 +0500

    bookings finalized

--- lib/view/pages/navbar/screens/booking_screen.dart
+++ lib/view/pages/navbar/screens/booking_screen.dart
@@ -5,7 +5,7 @@ import 'package:voyself_web/model/model.dart';
 import 'package:voyself_web/view/widgets/topbar.dart';
 
 class BookingScreen extends StatelessWidget {
-  BookingScreen({
+  const BookingScreen({
     super.key,
   });
 
@@ -23,96 +23,92 @@ class BookingScreen extends StatelessWidget {
             child: Table(
               children: [
                 TableRow(children: [
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Booking ID",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                    ),
+                  )),
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Client Name",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                    ),
+                  )),
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Booking For",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                    ),
+                  )),
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Guests",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
-                      "Check-in",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: h * 2.7,
-                          fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
-                      "Check-out",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: h * 2.7,
-                          fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                    ),
+                  )),
+                  TableCell(
+                      child: Text(
+                    "Check-in",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: h * 2.7,
+                        fontWeight: FontWeight.w600),
+                  )),
+                  TableCell(
+                      child: Text(
+                    "Check-out",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: h * 2.7,
+                        fontWeight: FontWeight.w600),
+                  )),
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Payment",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                    ),
+                  )),
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Status",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
-                  Center(
-                    child: TableCell(
-                        child: Text(
+                    ),
+                  )),
+                  TableCell(
+                      child: Center(
+                    child: Text(
                       "Date",
                       style: TextStyle(
                           color: Colors.white,
                           fontSize: h * 2.7,
                           fontWeight: FontWeight.w600),
-                    )),
-                  ),
+                    ),
+                  )),
                 ]),
               ],
             ),
@@ -146,87 +142,107 @@ class BookingScreen extends StatelessWidget {
                       Padding(
                         padding: EdgeInsets.symmetric(horizontal: w * 1),
                         child: Table(
+                          // defaultVerticalAlignment:
+                          // TableCellVerticalAlignment.middle,
                           children: [
                             TableRow(children: [
-                              Center(
-                                child: TableCell(
-                                    child: Text(
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   bookingDetails[index].bookingID.toString(),
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.5),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
-                                        bookingDetails[index].clientName,
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: h * 2.5))),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(bookingDetails[index].clientName,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: h * 2.5)),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   bookingDetails[index].bookingPlace,
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.5),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   bookingDetails[index].guest,
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.5),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   checkin,
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.5),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   checkout,
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.5),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   bookingDetails[index].payment,
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.7),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
-                                    child: Text(
-                                  bookingDetails[index].status
-                                      ? "Approved"
-                                      : "Pending",
-                                  style: TextStyle(
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Container(
+                                  height: h * 7,
+                                  width: w * 8,
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                        width: 2,
+                                        color: bookingDetails[index].status
+                                            ? Color.fromARGB(255, 0, 113, 4)
+                                            : Color.fromARGB(255, 255, 124, 16),
+                                      ),
+                                      borderRadius: BorderRadius.circular(15),
                                       color: bookingDetails[index].status
                                           ? const Color.fromARGB(
-                                              255, 119, 209, 122)
-                                          : Colors.yellow,
-                                      fontSize: h * 2.5,
-                                      fontWeight: FontWeight.w600),
-                                )),
-                              ),
-                              Center(
-                                child: TableCell(
+                                              255, 165, 216, 167)
+                                          : const Color.fromARGB(
+                                              255, 255, 242, 129)),
+                                  child: Center(
                                     child: Text(
+                                      bookingDetails[index].status
+                                          ? "Approved"
+                                          : "Pending",
+                                      style: TextStyle(
+                                          color: bookingDetails[index].status
+                                              ? Color.fromARGB(255, 0, 113, 4)
+                                              : Color.fromARGB(
+                                                  255, 255, 124, 16),
+                                          fontSize: h * 2.5,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                ),
+                              )),
+                              TableCell(
+                                  child: Center(
+                                child: Text(
                                   date,
                                   style: TextStyle(
                                       color: Colors.white, fontSize: h * 2.5),
-                                )),
-                              ),
+                                ),
+                              )),
                             ])
                           ],
                         ),
